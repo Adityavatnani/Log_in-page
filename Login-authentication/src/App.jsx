1a73ae26e0449ab0463c957041d39e06e04aa2fd
CodeVoyager07 <spawasthi93808@gmail.com>
2024-09-28 00:28:49 +0530

    update in html,css

--- Login-authentication/src/App.jsx
+++ Login-authentication/src/App.jsx
@@ -8,43 +8,44 @@ function App() {
 
   return (
     <>
-      <div className="container">
-              <div className="box">
-                <div className="form-sign-in">
+    <div class="container">
+        <div class="box">
+            <div class="form-container sign-in-container">
                 <form action="#">
-                  <h1>Login</h1>
-                  <input type="email" placeholder="Email" required />
-                  <button>Login</button>
-                  <p> Don't have an account? <a href="#" id="signuUp">Register</a></p> 
+                    <h1>Login</h1>
+                    <input type="email" placeholder="Email" required />
+                    <input type="password" placeholder="Password" required />
+                    <button>Login</button>
+                    <p>Don't have an account? <a href="#" id="signUp">Register</a></p>
                 </form>
-                </div>
-                <div className="form-sign-up">
-                  <form action="#">
+            </div>
+            <div class="form-container sign-up-container">
+                <form action="#">
                     <h1>Register</h1>
-                    <input type="text" placeholder='Name' required />
-                    <input type="email" placeholder='Email' required />
-                    <input type="password" placeholder='Password' required/>
+                    <input type="text" placeholder="Name" required />
+                    <input type="email" placeholder="Email" required />
+                    <input type="password" placeholder="Password" required />
                     <button>Register</button>
-                    <p>Already have an account? <a href="#" id='signIn'>Login</a></p>
-                  </form>
-                </div>
-                  <div className="over-container">
-                    <div className="over">
-                      <div className="over-panel-left">
+                    <p>Already have an account? <a href="#" id="signIn">Login</a></p>
+                </form>
+            </div>
+            <div class="overlay-container">
+                <div class="overlay">
+                    <div class="overlay-panel overlay-left">
                         <h1>Welcome Back!</h1>
                         <p>To keep connected with us please login with your personal info</p>
-                        <button className='ghost' id='signInOver'>Login</button>
-                      </div>
-                      <div className="over-panel-right">
+                        <button class="ghost" id="signInOverlay">Login</button>
+                    </div>
+                    <div class="overlay-panel overlay-right">
                         <h1>Hello, Friend!</h1>
                         <p>Enter your personal details and start your journey with us</p>
-                        <button className='ghost' id='signUpOver'>Register
-                        </button>
-                      </div>
+                        <button class="ghost" id="signUpOverlay">Register</button>
                     </div>
-                  </div>
-              </div>
+                </div>
             </div>
+        </div>
+    </div>
+
     </>
   )
 }
