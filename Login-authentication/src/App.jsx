14044a58fd3e7f6d91fdcf4869ef9547f98f6ef1
Adityavatnani <adityavatnani360@gmail.com>
2024-09-27 21:23:23 +0530

    App.jsx update

--- Login-authentication/src/App.jsx
+++ Login-authentication/src/App.jsx
@@ -8,7 +8,43 @@ function App() {
 
   return (
     <>
-      <h1>Aditya</h1>
+      <div className="container">
+              <div className="box">
+                <div className="form-sign-in">
+                <form action="#">
+                  <h1>Login</h1>
+                  <input type="email" placeholder="Email" required />
+                  <button>Login</button>
+                  <p> Don't have an account? <a href="#" id="signuUp">Register</a></p> 
+                </form>
+                </div>
+                <div className="form-sign-up">
+                  <form action="#">
+                    <h1>Register</h1>
+                    <input type="text" placeholder='Name' required />
+                    <input type="email" placeholder='Email' required />
+                    <input type="password" placeholder='Password' required/>
+                    <button>Register</button>
+                    <p>Already have an account? <a href="#" id='signIn'>Login</a></p>
+                  </form>
+                </div>
+                  <div className="over-container">
+                    <div className="over">
+                      <div className="over-panel-left">
+                        <h1>Welcome Back!</h1>
+                        <p>To keep connected with us please login with your personal info</p>
+                        <button className='ghost' id='signInOver'>Login</button>
+                      </div>
+                      <div className="over-panel-right">
+                        <h1>Hello, Friend!</h1>
+                        <p>Enter your personal details and start your journey with us</p>
+                        <button className='ghost' id='signUpOver'>Register
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
     </>
   )
 }
